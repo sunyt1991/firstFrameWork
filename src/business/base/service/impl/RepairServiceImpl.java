16c8ed3b50f9d923850280465a86efec1431b899
sunyt <sunqing971@163.com>
2016-01-18 21:19:16 +0800

    20160118

--- src/business/base/service/impl/RepairServiceImpl.java
+++ src/business/base/service/impl/RepairServiceImpl.java
@@ -10,7 +10,8 @@ public class RepairServiceImpl implements RepairService {
 
 	@Override
 	public void repair() {
-
+		System.out.println("数据修复开始。。。");
+		System.out.println("数据修复完毕。。。");
 	}
 
 	@Override
--- src/listener/OnlineListener.java
+++ src/listener/OnlineListener.java
@@ -1,66 +1,176 @@
 package listener;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletContextAttributeEvent;
 import javax.servlet.ServletContextAttributeListener;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRequestAttributeEvent;
+import javax.servlet.ServletRequestAttributeListener;
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionActivationListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionBindingListener;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
-import org.springframework.web.context.WebApplicationContext;
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import plug.SessionInfo;
+import util.ConfigUtil;
 
 /**
  * online user monitor
  * 
  */
-public class OnlineListener implements HttpSessionListener,
-		ServletContextListener, ServletContextAttributeListener {
+public class OnlineListener implements ServletContextListener, ServletContextAttributeListener, 
+	HttpSessionListener, HttpSessionAttributeListener, HttpSessionActivationListener, 
+	HttpSessionBindingListener, ServletRequestListener, ServletRequestAttributeListener {
 
-	private ServletContext context = null;
+	
+	private static final Logger logger = Logger.getLogger(OnlineListener.class);
 
-	private WebApplicationContext webContext = null;
+	private static ApplicationContext ctx = null;
 
-	// context初始化时激发
-
-	public void contextInitialized(ServletContextEvent event) {
-		System.out.println("context初始化");
+	public OnlineListener() {}
+	
+	@Override
+	public void attributeAdded(ServletRequestAttributeEvent evt) {
+		// TODO Auto-generated method stub
 	}
 
 	@Override
-	public void attributeAdded(ServletContextAttributeEvent arg0) {
+	public void attributeRemoved(ServletRequestAttributeEvent arg0) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
-	public void attributeRemoved(ServletContextAttributeEvent arg0) {
+	public void attributeReplaced(ServletRequestAttributeEvent arg0) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
-	public void attributeReplaced(ServletContextAttributeEvent arg0) {
+	public void requestDestroyed(ServletRequestEvent arg0) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
-	public void contextDestroyed(ServletContextEvent arg0) {
+	public void requestInitialized(ServletRequestEvent arg0) {
 		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public void valueBound(HttpSessionBindingEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void valueUnbound(HttpSessionBindingEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sessionDidActivate(HttpSessionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sessionWillPassivate(HttpSessionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * 添加键值对到session中时的回调函数(例如:登录成功)
+	 */
+	@Override
+	public void attributeAdded(HttpSessionBindingEvent evt) {
+		String name = evt.getName();
+		logger.debug("The name of attribute saved in session: " + name);
+		if (ConfigUtil.getSessionInfoName().equals(name)) {
+			HttpSession session = evt.getSession();
+			SessionInfo sessionInfo = (SessionInfo) session.getAttribute(name);
+			if (sessionInfo != null) {
+				//OnlineServiceI onlineService = (OnlineServiceI) ctx.getBean("onlineService");
+				//onlineService.saveOrUpdateTonlineByLoginNameAndIp(sessionInfo.getLoginName(), sessionInfo.getIp());
+			}
+		}
+		
+	}
+
+	@Override
+	public void attributeRemoved(HttpSessionBindingEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void attributeReplaced(HttpSessionBindingEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
 	public void sessionCreated(HttpSessionEvent arg0) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void sessionDestroyed(HttpSessionEvent arg0) {
 		// TODO Auto-generated method stub
-
+		
 	}
+
+	@Override
+	public void attributeAdded(ServletContextAttributeEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void attributeRemoved(ServletContextAttributeEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void attributeReplaced(ServletContextAttributeEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * 系统销毁时回调
+	 */
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		logger.debug("server closed");
+		System.out.println("系统销毁");
+	}
+
+	/**
+	 * 系统启动时回调
+	 */
+	@Override
+	public void contextInitialized(ServletContextEvent evt) {
+		logger.debug("server started");
+		ctx = WebApplicationContextUtils.getWebApplicationContext(evt.getServletContext());
+		System.out.println("系统启动:"+ctx);
+	}
+
+	
+	
 
 }
